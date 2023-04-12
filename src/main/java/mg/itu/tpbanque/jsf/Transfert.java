1877d4527625255e342912df3a5633862c1f1eb6
Mel <asimbolalalaina@gmail.com>
2023-04-12 17:10:38 +0300

    Messages pour les transferts

--- src/main/java/mg/itu/tpbanque/jsf/Transfert.java
+++ src/main/java/mg/itu/tpbanque/jsf/Transfert.java
@@ -5,13 +5,12 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 import mg.itu.tpbanque.ejb.GestionnaireCompte;
 import mg.itu.tpbanque.entities.CompteBancaire;
+import mg.itu.tpbanque.jsf.util.Util;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
  */
-
-
 /**
  *
  * @author Mel
@@ -19,10 +18,11 @@ import mg.itu.tpbanque.entities.CompteBancaire;
 @Named(value = "transfert")
 @RequestScoped
 public class Transfert {
+
     private int idSource;
     private int idDestinataire;
     private int montant;
-    
+
     @EJB
     GestionnaireCompte gestionnaire;
 
@@ -55,12 +55,37 @@ public class Transfert {
     public void setMontant(int montant) {
         this.montant = montant;
     }
-    
-    public String transferer(){
+
+    public String transferer() {
+        boolean erreur = false;
         CompteBancaire source = this.gestionnaire.getCompteById(idSource);
+        if (source == null) {
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (source.getSolde() < montant) { // à compléter pour le cas où le solde du compte source est insuffisant...
+                Util.messageErreur("Votre solde est insuffisant !", "Solde insuffisant", "form:montant");
+                erreur = true;
+            }
+        }
+        if(erreur == true){
+            return null;
+        }
         CompteBancaire destinataire = this.gestionnaire.getCompteById(idDestinataire);
+        if (destinataire == null) {
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destinataire");
+            erreur = true;
+        }
+        if(erreur == true){
+            return null;
+        }
         this.gestionnaire.transferer(source, destinataire, montant);
+        Util.addFlashInfoMessage("Tranfert correctement effectué : " + 
+                source.getNom() +
+                " a transféré " +
+                montant + 
+                " à " + destinataire.getNom());
         return "listeComptes?faces-redirect=true";
     }
-    
+
 }
