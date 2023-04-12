56bcbe32fb51e5af7d54ff271b9d6f6d22d5d4e4
Mel <asimbolalalaina@gmail.com>
2023-04-12 16:01:37 +0300

    Affichage Liste des Comptes

---
+++ src/main/java/mg/itu/tpbanque/jsf/ListeComptes.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanque.jsf;
+
+import jakarta.ejb.EJB;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanque.ejb.GestionnaireCompte;
+import mg.itu.tpbanque.entities.CompteBancaire;
+
+/**
+ *
+ * @author Mel
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+    private List<CompteBancaire> allComptes;
+    
+    @EJB
+    GestionnaireCompte gestionnaire;
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+    
+    public List<CompteBancaire> getAllComptes(){
+        if(this.allComptes == null){
+            this.allComptes = this.gestionnaire.getAllComptes();
+        }
+        return this.allComptes;
+    }
+    
+}
